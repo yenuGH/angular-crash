8ea65cc3ea5e8ce8b1c7619b6a5e057415156578
yenu <35312312+yenuGH@users.noreply.github.com>
2023-11-17 18:29:43 -0800

    create a local server, create get and delete requests to the local server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit{
 
-    tasks: Task[] = TASKS;
+    tasks: Task[] = [];
 
+    constructor(private taskService: TaskService) {
+
+    }
+
+    ngOnInit(): void {
+        this.taskService.getTasks().subscribe(
+            (tasks) => this.tasks = tasks
+        );
+    }
+
+    deleteTask(task: Task) {
+        this.taskService.deleteTask(task).subscribe(
+            // for each task in tasks, return only the tasks that don't have the id of the task that was deleted
+            () => this.tasks = this.tasks.filter(t => t.id !== task.id)
+        );
+    }
 }
