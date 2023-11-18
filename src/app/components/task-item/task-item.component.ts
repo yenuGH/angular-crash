0103860ad7e8180c03bc0fa56fcf3a845d74ac11
yenu <35312312+yenuGH@users.noreply.github.com>
2023-11-17 17:19:32 -0800

    add a delete button to each task

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -8,4 +9,5 @@ import { Task } from '../../Task';
 })
 export class TaskItemComponent {
     @Input() task: Task;
+    faTimes = faTimes;
 }
